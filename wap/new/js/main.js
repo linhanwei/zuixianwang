a5b96710b4eb5d03d41dc8986b1124ad39ebb713
linhanwei <2757104556@qq.com>
2016-11-02 14:34:34 +0800

    修改首页

--- wap/new/js/main.js
+++ wap/new/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
-  
+   var swiper = new Swiper('.swiper-container', {
+	//pagination: '.swiper-pagination',
+	nextButton: '.swiper-button-next',
+	prevButton: '.swiper-button-prev',
+	paginationClickable: true,
+	spaceBetween: 30,
+	centeredSlides: true,
+	autoplay: 2500,
+	autoplayDisableOnInteraction: false
+   });	
+   
+   //图片延迟加载
+   $("img.lazy").lazyload({effect: "fadeIn",threshold:"400"});
+   
    $(".tab-nav").find('li').click(function(){
 	  if($(this).hasClass('action')){
 		  return false;
@@ -11,8 +24,7 @@ $(function(){
 		  $(".detail-content-box .content-item").eq(index).addClass("action");
 	  }  
    })
-   
-   
+
    $(".chanle-list .more").click(function(){ 
 	   var cb = $(".chanle-list .cate-child-box");
 	   var cv = $(".chanle-list .cate-child-v");
@@ -37,4 +49,39 @@ $(function(){
           $('body,html').animate({scrollTop:0},500);
               return false;
    });
-})
+
+	//搜索
+	$('.search-btn').click(function(){
+		var keyword = encodeURIComponent($('#keyword').val());
+		location.href = SiteUrl+'/mall_m/index.php?act=goods&op=list&keyword='+keyword;
+	});
+
+	//修改a链接增加key值
+	add_key();
+});
+
+//修改a链接增加key值
+function add_key(){
+	$('a').each(function(k,v){
+		var key = getcookie('key'),
+			a_href = $(v).attr('href'),
+			new_href = '';
+
+		if(a_href && a_href.indexOf("javascript") < 0){
+			var key_index = a_href.indexOf("?");
+			if(key_index >= 0){
+				if(a_href.indexOf("=") >= 0){
+					new_href = a_href+'&key='+key;
+				}else{
+					new_href = a_href+'key='+key;
+				}
+			}else{
+				new_href = a_href+'?key='+key;
+			}
+
+			$(v).attr('href',new_href);
+			//console.log(key,key_index,k,a_href,111);
+		}
+	});
+}
+
