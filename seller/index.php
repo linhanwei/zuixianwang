1168b87a45cfd946b5bd19882efe33a2bebbb6da
linhanwei <2757104556@qq.com>
2016-11-14 10:30:56 +0800

    修改商户后台

--- seller/index.php
+++ seller/index.php
@@ -5,10 +5,10 @@ define('BASE_PATH',str_replace('\\','/',dirname(__FILE__)));
 if (!@include(dirname(dirname(__FILE__)).'/global.php')) exit('global.php isn\'t exists!');
 if (!@include(BASE_PATH.'/control/control.php')) exit('control.php isn\'t exists!');
 if (!@include(BASE_CORE_PATH.'/site.php')) exit('site.php isn\'t exists!');
-define('APP_SITE_URL',MALL_SITE_URL);
+define('APP_SITE_URL',SELLER_SITE_URL);
 define('TPL_NAME','default');
-define('MALL_RESOURCE_SITE_URL',MALL_SITE_URL.DS.'resource');
-define('MALL_TEMPLATES_URL',MALL_SITE_URL.'/templates/'.TPL_NAME);
+define('MALL_RESOURCE_SITE_URL',SELLER_SITE_URL.DS.'resource');
+define('MALL_TEMPLATES_URL',SELLER_SITE_URL.'/templates/'.TPL_NAME);
 define('BASE_TPL_PATH',BASE_PATH.'/templates/'.TPL_NAME);
 
 Base::run();
