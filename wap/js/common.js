fbe9f32497050fbdca3b0d8416dc12aedbd5cd1b
linhanwei <2757104556@qq.com>
2016-11-17 17:33:54 +0800

    修改添加搜索商品与更改密码页面

--- wap/js/common.js
+++ wap/js/common.js
@@ -67,31 +67,37 @@ function GetQueryString(name){
 }
 
 function addcookie(name,value,expireHours){
-    var cookieString=name+"="+escape(value)+"; path=/";
+    setCache(name,value);
+    return false;
+   /* var cookieString=name+"="+escape(value)+"; path=/";
     //判断是否设置过期时间
     if(expireHours>0){
         var date=new Date();
         date.setTime(date.getTime+expireHours*3600*1000);
         cookieString=cookieString+"; expire="+date.toGMTString();
     }
-    document.cookie=cookieString;
+    document.cookie=cookieString;*/
 }
 
 function getcookie(name){
+    return getCache(name);
+    /*
     var strcookie=document.cookie;
     var arrcookie=strcookie.split("; ");
     for(var i=0;i<arrcookie.length;i++){
         var arr=arrcookie[i].split("=");
         if(arr[0]==name)return arr[1];
     }
-    return "";
+    return "";*/
 }
 
 function delCookie(name){//删除cookie
+    return delCache(name);
+/*
     var exp = new Date();
     exp.setTime(exp.getTime() - 1);
     var cval=getcookie(name);
-    if(cval!=null) document.cookie= name + "="+cval+"; path=/;expires="+exp.toGMTString();
+    if(cval!=null) document.cookie= name + "="+cval+"; path=/;expires="+exp.toGMTString();*/
 }
 
 function checklogin(state){
