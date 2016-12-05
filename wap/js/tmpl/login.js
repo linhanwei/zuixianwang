97c4d5316853c2c780f96eb2f4de8eaf7ddb9089
linhanwei <2757104556@qq.com>
2016-12-05 09:57:43 +0800

    修改个人资料

--- wap/js/tmpl/login.js
+++ wap/js/tmpl/login.js
@@ -9,27 +9,6 @@ $(function(){
 	var referurl = document.referrer;//上级网址
 
 	$("input[name=referurl]").val(referurl);
-	$.sValid.init({
-        rules:{
-            username:"required",
-            userpwd:"required"
-        },
-        messages:{
-            username:"用户名必须填写！",
-            userpwd:"密码必填!"
-        },
-        callback:function (eId,eMsg,eRules){
-            if(eId.length >0){
-                var errorHtml = "";
-                $.map(eMsg,function (idx,item){
-                    errorHtml += "<p>"+idx+"</p>";
-                });
-                $(".error-tips").html(errorHtml).show();
-            }else{
-                 $(".error-tips").html("").hide();
-            }
-        }  
-    });
 
     //记住密码
     $('.rember-pass-box').click(function(){
@@ -43,41 +22,40 @@ $(function(){
     $('#username').val(getCache('user_name')?getCache('user_name'):'');
     $('#userpwd').val(getCache('user_password')?getCache('user_password'):'');
 	$('#loginbtn').click(function(){//会员登陆
-		var username = $('#username').val();
-		var pwd = $('#userpwd').val();
-		var client = 'wap';
-		if($.sValid()){
-	          $.ajax({
-				type:'post',
-				url:ApiUrl+"/index.php?act=login",	
-				data:{username:username,password:pwd,client:client},
-				dataType:'json',
-				success:function(result){
-					if(!result.datas.error){
-						if(typeof(result.datas.key)=='undefined'){
-							return false;
-						}else{
-							addcookie('username',result.datas.username);
-							addcookie('key',result.datas.key);
-                            setCache('user_name',username);
-                            if($('.rember-pass-box').hasClass('action')){
-                                setCache('user_password',pwd);
-                            }else{
-                                setCache('user_password','');
-                            }
-                            if(typeof(app_interface) == 'object'){
-                                app_interface.closeWebView(8,getQueryString('func') + '();');
-                            }else{
-                                location.href = referurl;
-                            }
-						}
-						$(".error-tips").hide();
-					}else{
-						$(".error-tips").html(result.datas.error).show();
-					}
-				}
-			 });  
-        }
-	});
+        var username = $('#username').val();
+        var pwd = $('#userpwd').val();
+        var client = 'wap';
+        if(username == '') {app_toast('请输入用户名');$('#username').focus();return;}
+        if(pwd == '') {app_toast('请输入密码');$('#userpwd').focus();return;}
+          $.ajax({
+            type:'post',
+            url:ApiUrl+"/index.php?act=login",
+            data:{username:username,password:pwd,client:client},
+            dataType:'json',
+            success:function(result){
+                if(!result.datas.error){
+                    if(typeof(result.datas.key)=='undefined'){
+                        return false;
+                    }else{
+                        addcookie('username',result.datas.username);
+                        addcookie('key',result.datas.key);
+                        setCache('user_name',username);
+                        if($('.rember-pass-box').hasClass('action')){
+                            setCache('user_password',pwd);
+                        }else{
+                            setCache('user_password','');
+                        }
+                        if(typeof(app_interface) == 'object'){
+                            app_interface.closeWebView(8,getQueryString('func') + '();');
+                        }else{
+                            location.href = referurl;
+                        }
+                    }
+                }else{
+                    app_toast(result.datas.error);
+                }
+            }
+         });
+    });
 
 });
